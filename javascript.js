5429065165a9423e5db2455a40684cf00fb7d215
JoaoNer <johnnercastrojy@gmail.com>
2022-02-12 21:10:02 -0300

    corrigindo erro

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function verificar() {
     var data = new Date()
-    var ano = data.getFullYear() // pega os quatro digitos do ano
+    var ano = data.getFullYear() //pega os quatro digitos do ano
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     if (Number(fano.value.length == 0 || Number(fano.value > ano))){
